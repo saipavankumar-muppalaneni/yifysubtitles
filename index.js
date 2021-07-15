48c6ae5072ff2d5b9cd80fe7cb4240ebc9e9343d
saipavankumar-muppalaneni <46277003+saipavankumar-muppalaneni@users.noreply.github.com>
2021-07-16 02:34:44 +0530

    Update index.js
    domain change

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const cheerio = require('cheerio');
 const langsFormat = require('./langs');
 
 // Down const apiUri = 'http://api.yifysubtitles.com/subs';
-const uri = 'https://www.yifysubtitles.com/movie-imdb';
-const downloadUri = 'https://yifysubtitles.com';
+const uri = 'https://www.yifysubtitles.org/movie-imdb';
+const downloadUri = 'https://yifysubtitles.org';
 const langK = Object.keys(langsFormat);
 const langV = langK.map(i => langsFormat[i]);
 
